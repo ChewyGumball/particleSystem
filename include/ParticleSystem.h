4829430661619a610aad6434dba27dfa880655f8
Ben Dunkin <chewygumball@gmail.com>
2013-04-18 21:30:22 -0400

    Initial commit

---
+++ include/ParticleSystem.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <glm/glm.hpp>
+
+class ParticleSystem
+{
+	glm::vec4 position;
+};
---
+++ src/ParticleSystem.cpp
@@ -0,0 +1,14 @@
+// particleSystem.cpp : Defines the entry point for the console application.
+//
+
+#include <iostream>
+
+#include "ParticleSystem.h"
+
+
+int main(int argc, char* argv[])
+{
+	std::cout << "HelloWorld" << std::endl;
+	return 0;
+}
+
